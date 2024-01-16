5b7dc9165a69c7566477dc20ad542d6bb5da96a7
CamiloP121 <andresospino14@gmail.com>
2024-01-16 14:29:50 -0500

    add regrex email

--- utils/ETL.py
+++ utils/ETL.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import pycountry
+import re
+from utils.helpers import *
 
 def dict2dataframe(dict) -> pd.DataFrame:
     """
@@ -20,3 +23,46 @@ def dict2dataframe(dict) -> pd.DataFrame:
     ]
 
     return pd.DataFrame(temporal_vector_data)
+
+def found_contry(country: str):
+    """
+    Search for a country and/or city using a fuzzy search with the pycountry library.
+    ------------------------------------------------------------------
+    Parameters:
+    - country (str): The name of the country to search for
+    Returns:
+    - tuple: A tuple containing the found country name and city
+    """
+    try:
+        response = pycountry.countries.search_fuzzy(country)
+        country_search = response[0].name
+    except Exception as e:
+        printr(e)
+        country_search = "error" 
+
+    city = ""
+
+    if country_search != country:
+        city = country
+    
+    return country_search, country
+
+
+def find_email(txt: str) -> str:
+    """
+    Extracts an email address from the given text using a regular expression.
+    -------------------------------------------------------------------
+    Arg:
+    - txt (str): The input text from which to extract the email address
+    Returns:
+    - str: The extracted email address, or "Error" if no email address is found
+    """
+    
+    regular_expression = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    try:
+        # Use re.findall to find all email addresses in the text, and take the first one
+        emails_found = re.findall(regular_expression, txt)[0]
+    except Exception as e:
+        print("Error:", e)
+        emails_found = "Error"
+    return emails_found
