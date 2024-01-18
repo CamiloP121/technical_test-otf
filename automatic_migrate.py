2367a7719383de24a674fe4955b4aab187da5654
CamiloP121 <andresospino14@gmail.com>
2024-01-17 19:39:41 -0500

    transform data

--- automatic_migrate.py
+++ automatic_migrate.py
@@ -4,9 +4,8 @@ import numpy as numpy
 import os
 import json
 from pathlib import Path
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from datetime import datetime
-from IPython.display import clear_output
 
 # Own functions
 from models import query, ETL
@@ -29,13 +28,36 @@ def automatic_migrate_source2accoutn_otf(API_KEY_SOURCE:str,
     
     for name, itmes in keys.items():
         assert isinstance(itmes["api_key"], str), f"{name} not is a string"
-        printy(F"Review ststus API {name} ...")
+        printy(F"Review ststus {name} ...")
         if utils.check_api_status(**itmes):
             printg(f"Correctly connection with {name}")
         else:
             raise Exception(f"Error connection with {name}")
 
-
+    print("** Continue for extract source data\n")
+    
+    # Crate checkpoint folder
+    Path('checkpoint').mkdir(parents=True, exist_ok=True)
+    data_path = "checkpoint/data.csv"
+    printy("Start search")
+    # Extract first page
+    dict_data_result = query.searchAPI(keys["API_SOURCE"]["api_key"],)
+    # Convert in DataFrame
+    data = [ETL.dict2dataframe(dict_data_result)]
+    # Partition
+    data_size = dict_data_result["total"]
+    iterations = data_size // 100 if data_size % 100 == 0 else (data_size // 100) + 1
+    limit = 100
+    for i in tqdm(range(1,iterations), desc="Extracting"):
+        if data_size - (i*100) <= 100:
+            limit = data_size - (i*100)
+        dict_data_result = query.searchAPI(keys["API_SOURCE"]["api_key"], limit = limit, page = i)
+        # Convert in DataFrame
+        data.extend([ETL.dict2dataframe(dict_data_result)])
+    printg("Finish search")
+    # Save in checkpoint
+    data = pd.concat(data)
+    data.to_csv(data_path, sep = "|", index = False)
 
 
 
